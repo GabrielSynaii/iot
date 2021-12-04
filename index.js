bbc0ef25fe51c9b2d0fb3ff715c422a0cd6bb62e
Gabriel <gabrielsynaii@gmail.com>
2021-12-04 18:35:54 -0300

    Add sample broker

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 require("dotenv").config();
+
+const mqtt = require("mqtt");
+
+const client = mqtt.connect(
+  `mqtt://${process.env.BROKER_IP}:${process.env.BROKER_PORT}`
+);
+
+client.on("connect", () => {
+  client.subscribe("test");
+});
+
+client.on("message", (topic, message) => {
+  console.log({ topic, message: message.toString() });
+});
