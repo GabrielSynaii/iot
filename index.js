3184b7eda2b4dbeaed332d607e8b63c57c5b38a4
Gabriel <gabrielsynaii@gmail.com>
2021-12-04 19:42:32 -0300

    Save messages to influx

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 
 const mqtt = require("mqtt");
+const { InfluxDB } = require("influx");
+
+const influx = new InfluxDB(
+  `http://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+);
 
 const client = mqtt.connect(
   `mqtt://${process.env.BROKER_IP}:${process.env.BROKER_PORT}`
@@ -11,9 +16,19 @@ client.on("connect", () => {
 });
 
 client.on("message", (topic, message) => {
+  console.log(
+    `[${new Date().toLocaleTimeString()}] - ${topic} received ${message}`
+  );
+
   switch (topic) {
     case "iot/led":
-      console.log({ topic, message: message.toString() });
+      influx.writeMeasurement("led", [
+        {
+          fields: {
+            value: message,
+          },
+        },
+      ]);
       break;
   }
 });
